8963385a232e8e8b291ae6a0499abfa1feeb35a6
Harmelodic <matt@harmelodic.com>
2022-05-02 23:22:26 +0200

    GitHub Actions and tidying client build code

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,24 +1,13 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const config = require('./webpack.config');
-const path = require('path');
 
 module.exports = merge(config, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: {
-      index: 'index.dev.html'
-    },
-    hot: true,
-    index: 'index.dev.html',
-    inline: true,
-    port: 8080
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      API_SERVER: 'http://localhost:3000'
-    })
-  ]
+	mode: 'development',
+	devtool: 'inline-source-map',
+	plugins: [
+		new webpack.EnvironmentPlugin({
+			API_SERVER: 'http://localhost:3000'
+		})
+	]
 });
--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,22 +1,20 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
-const MinifyPlugin = require('babel-minify-webpack-plugin');
+const { merge } = require('webpack-merge');
 const CompressionPlugin = require('compression-webpack-plugin');
 const config = require('./webpack.config');
 
 module.exports = merge(config, {
-  mode: 'production',
-  plugins: [
-    new MinifyPlugin(),
-    new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-    new webpack.EnvironmentPlugin({
-      API_SERVER: '/api'
-    })
-  ],
+	mode: 'production',
+	plugins: [
+		new CompressionPlugin({
+			filename: '[path][base].gz[query]',
+			algorithm: 'gzip',
+			test: /\.js$|\.css$|\.html$/,
+			threshold: 10240,
+			minRatio: 0.8,
+		}),
+		new webpack.EnvironmentPlugin({
+			API_SERVER: '/api'
+		})
+	],
 });
