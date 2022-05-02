9d7af77311b50312962677f12a1c2294c0498589
Harmelodic <matt@harmelodic.com>
2022-05-02 23:30:30 +0200

    Removing harmelodic/interfaces

--- src/interfaces/db.js
+++ src/interfaces/db.js
@@ -1,6 +1,38 @@
-import {sql} from '@harmelodic/interfaces';
+const mysql = require('mysql');
 
-export default sql({
+const sql = {};
+let pool;
+
+sql.close = () => {
+    if (pool) pool.end();
+    pool = null;
+};
+
+sql.query = (sql) => {
+    return new Promise(((resolve, reject) => {
+        pool.getConnection((err, con) => {
+            if (err) reject(err);
+
+            con.query(sql, (error, results) => {
+                if (error) reject(error);
+
+                con.release();
+                resolve(results);
+            });
+        });
+    }));
+};
+
+process.on('beforeExit', () => {
+    sql.close();
+});
+
+const db = (config) => {
+    pool = mysql.createPool(config);
+    return sql;
+}
+
+module.exports = db({
     host: '127.0.0.1',
     port: 3306,
     database: 'shopping',
